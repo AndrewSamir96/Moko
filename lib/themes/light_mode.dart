e0de599e954fc3c34db5cb4b7c7a8e905b77da70
Andrew Samir <87244606+AndrewSamir96@users.noreply.github.com>
2024-03-11 17:35:28 +0200

    Add files via upload

---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  useMaterial3: true,
+  fontFamily: 'Montserrat',
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    inversePrimary: Colors.grey.shade900,
+    
+  ),
+);
